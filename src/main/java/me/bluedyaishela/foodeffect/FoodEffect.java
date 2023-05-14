5d54644b8d2ae89fe990d419e4104bef45f412e2
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-14 21:58:24 +0200

    init

---
+++ src/main/java/me/bluedyaishela/foodeffect/FoodEffect.java
@@ -0,0 +1,17 @@
+package me.bluedyaishela.foodeffect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FoodEffect extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
